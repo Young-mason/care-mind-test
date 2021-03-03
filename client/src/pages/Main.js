1744f6d88ffc164a2b9aece4b0bcfe74b2b1648e
Young-Mason <mason.yk3@gmail.com>
2021-03-03 16:40:22 +0900

    Added #2-2 chart

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import GoogleChart from "../componenets/GoogleChart";
 import FontSizer from "../componenets/FontSizer";
 import Greeter from "../componenets/Greeter";
 
 function Main({ showNav }) {
   const [fontSize, setFontSize] = useState(16);
+  console.log(fontSize);
 
   return (
     <main
@@ -16,6 +18,7 @@ function Main({ showNav }) {
 
       <Greeter user="Master"></Greeter>
       <p>1-1, 1-2, 2-1 문항을 이 페이지에 구현하였습니다</p>
+      <GoogleChart></GoogleChart>
     </main>
   );
 }
