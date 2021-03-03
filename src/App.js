66e409d39b1a61d02d5d44a38dddafe666f9f015
Young-Mason <mason.yk3@gmail.com>
2021-03-03 22:41:42 +0900

    fix bugs

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Main from "./pages/Main";
+import { useState } from "react";
+
+function App() {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <div className="App">
+      <Navbar showNav={showNav} setShowNav={setShowNav}></Navbar>
+      <Main showNav={showNav}></Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Main.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import GoogleChart from "../components/GoogleChart";
+import FontSizer from "../components/FontSizer";
+import Greeter from "../components/Greeter";
+
+function Main({ showNav }) {
+  const [fontSize, setFontSize] = useState(16);
+  console.log(fontSize);
+
+  return (
+    <main
+      className={showNav ? "show-nav" : ""}
+      style={{
+        fontSize: `${fontSize.toString()}px`,
+      }}
+    >
+      <FontSizer fontSize={fontSize} setFontSize={setFontSize}></FontSizer>
+      <Greeter user="Master"></Greeter>
+      <GoogleChart></GoogleChart>
+    </main>
+  );
+}
+
+export default Main;
